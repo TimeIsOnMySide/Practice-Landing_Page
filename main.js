f0d997f5a066a5a96cb32081f2a5893831288e49
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-01-29 12:13:41 -0800

    Progress from yesterday and today

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+//Watch the button. Wait for a click
 document.querySelector('#show-hide-textbox').addEventListener('click', showForm)
 
 //Hide element on page load
 document.getElementById('email-form').hidden = true;
+//document.getElementById('show-hide-textbox').hidden = true;
 
-
+//When the button is clicked, hide the button and show the form
 function showForm() {
     document.getElementById('email-form').hidden = false;
     document.getElementById('show-hide-textbox').hidden = true;
