30f71c5bb9a341ab9d44a883cd9cf3e9a6a9f890
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-01-29 15:12:46 -0800

    got smooth scroll to anchor to work!

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
+//Sign up button!
+
 //Watch the button. Wait for a click
 document.querySelector('#show-hide-textbox').addEventListener('click', showForm)
 
 //Hide element on page load
 document.getElementById('email-form').hidden = true;
-//document.getElementById('show-hide-textbox').hidden = true;
 
 //When the button is clicked, hide the button and show the form
 function showForm() {
     document.getElementById('email-form').hidden = false;
     document.getElementById('show-hide-textbox').hidden = true;
 }
+
+//======================================================================
+
+//Smooth Scrolling
+
+function scrollFunction1() {
+    let e = document.getElementById('section1');
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'start'
+    });
+  }
+
+  function scrollFunction2() {
+    let e = document.getElementById('section2');
+    // This ends the block to the window 
+    // bottom and also aligns the view to the center 
+    e.scrollIntoView({
+      block: 'end',
+      behavior: 'smooth',
+      inline: 'center'
+    });
+  }
